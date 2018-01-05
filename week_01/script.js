7776453c0e693c841880bf38dfa5984689d3a098
Michael Nomura <mnnomura@gmail.com>
2018-01-04 19:59:27 -0800

    changes directory case

--- week_01/script.js
+++ week_01/script.js
@@ -1,4 +1,4 @@
 var firstName = "Michael";
 var lastName = "Nomura";
 var yearsOfStudy = "one year";
-var goal = "To obtain a job in web development";
+var goal = "to obtain a job in web development";
